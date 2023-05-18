2b94ff533691f42febe5d2978889446cfe51a019
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-18 23:00:16 +0600

    login register page added

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+             <h2 className="text-4xl">Please Login</h2>
+        </div>
+    );
+};
+
+export default Register;
