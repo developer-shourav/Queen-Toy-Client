8ab747ad89db02d35c4d70e12ce554876bb1e5b4
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-21 12:30:03 +0600

    content updated

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import TopBanner from "../TopBanner/TopBanner";
 
 const Home = () => {
   /* -------------Dynamic Title Adding hook---------- */
   return (
     <div className="container mx-auto">
       {/* -----------Top Banner Section---------- */}
+      <TopBanner> </TopBanner>
 
 
      
--- src/pages/Home/TopBanner/TopBanner.jsx
+++ src/pages/Home/TopBanner/TopBanner.jsx
@@ -5,19 +5,24 @@ import SliderImg2 from "../../../assets/images/banner-img/sindralla2.png";
 const TopBanner = () => {
   return (
     <div className="container mx-auto">
+
+      {/* -----------First Banner Section------------ */}
       <div>
         <div className="">
+          {/* -------Main Heading------- */}
           <h2 className="font-extrabold text-5xl text-center mt-8 logo-text-shadow  logo-style">
             Welcome To{" "}
             <span className=" text-pink-500 custom-text-shadow">Queen Toy</span>
           </h2>
         </div>
 
+       {/*--------First Full Image ------------  */}
         <div>
           <img className="w-10/12 mx-auto" src={Banner} alt="" />
         </div>
       </div>
 
+    {/* ------------Second Image Section ---------------- */}
       <div className="flex justify-between items-start gap-10 mt-20">
         <div className="lg:w-1/2 ">
           <img src={SliderImg1} className="w-8/12 " alt="" />
@@ -29,33 +34,31 @@ const TopBanner = () => {
           <h2 className="font-bold text-5xl text-pink-600 custom-text-shadow ">
             Disney dolls Toy Seller
           </h2>
-          <p className="w-3/4 my-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            obcaecati maiores quae dicta atque pariatur labore officia alias
-            neque repellendus!
+          <p className="w-3/4 my-5 text-justify ">
+          We have all kinds of Disney doll toys. You will get exactly what you want and what you are looking for. We always provide the best product to our customers. You are in the right place for choices the best.
           </p>
 
-          <button className="btn rounded  bg-pink-500 border-0  hover:bg-pink-700 ">
-            See Gallery
+          <button className="btn rounded  bg-[#fd598f] shadow-md shadow-[#993153] border-0  hover:bg-pink-700 ">
+            Buy Toys
           </button>
         </div>
       </div>
 
+
+        {/* ---------Third Image Section -------------- */}
       <div className="flex mx-auto justify-between items-start gap-10 mt-20">
         <div className="lg:w-1/2 mt-16 ps-12">
-          <h3 className="font-bold text-4xl my-3 ">We Are the best</h3>
+          <h3 className="font-bold text-4xl my-3 ">Get The Toy You want</h3>
 
           <h2 className="font-bold text-5xl logo-text-shadow text-[#18b1b9] ">
-            Disney dolls Toy Seller
+            Find Your Disney dolls
           </h2>
           <p className="w-3/4 my-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            obcaecati maiores quae dicta atque pariatur labore officia alias
-            neque repellendus!
+          We have all kinds of Disney doll toys. You will get exactly what you want and what you are looking for. We always provide the best product to our customers. You are in the right place for choices the best.
           </p>
 
-          <button className="btn rounded bg-cyan-500 border-0 hover:bg-cyan-700">
-            Buy Toys
+          <button className="btn rounded bg-[#0ec3cc] shadow-md shadow-[#266e72] border-0 hover:bg-cyan-700">
+          See Gallery
           </button>
         </div>
 
