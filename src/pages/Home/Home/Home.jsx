a6cceea4cefe34c0d3bbfd8e9928fd39ff528ce6
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-21 13:47:31 +0600

    gallery component added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TopBanner from "../TopBanner/TopBanner";
-
+import BorderB from '../../../assets/images/border-img/borderB.svg';
 const Home = () => {
   /* -------------Dynamic Title Adding hook---------- */
   return (
@@ -8,7 +8,11 @@ const Home = () => {
       {/* -----------Top Banner Section---------- */}
       <TopBanner> </TopBanner>
 
-
+      {/* -----------Top Banner Section---------- */}
+      <div className=" mt-16 md:mt-20 lg:mt-28 text-center">
+        <h2 className=" text-2xl md:text-3xl lg:text-4xl text-center font-bold">Our Gallery</h2>
+        <img src={BorderB} className="text-center w-3/6 md:w-4/12 lg:w-3/12 mx-auto" alt="" />
+      </div>
      
     </div>
   );
