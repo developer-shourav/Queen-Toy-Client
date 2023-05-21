8b27b2795a48fbcac80324ceb5cc8a6b35089454
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-21 15:00:12 +0600

    photo gallery complete

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopBanner from "../TopBanner/TopBanner";
 import BorderB from '../../../assets/images/border-img/borderB.svg';
+import PhotoGallery from "../PhotoGallery/PhotoGallery";
 const Home = () => {
   /* -------------Dynamic Title Adding hook---------- */
   return (
@@ -8,10 +9,15 @@ const Home = () => {
       {/* -----------Top Banner Section---------- */}
       <TopBanner> </TopBanner>
 
-      {/* -----------Top Banner Section---------- */}
-      <div className=" mt-16 md:mt-20 lg:mt-28 text-center">
+      {/* -----------Photo Gallery Section---------- */}
+      <div className=" mt-16 md:mt-20 lg:mt-28 ">
+
+        {/* ----------Heading Style--------- */}
         <h2 className=" text-2xl md:text-3xl lg:text-4xl text-center font-bold">Our Gallery</h2>
         <img src={BorderB} className="text-center w-3/6 md:w-4/12 lg:w-3/12 mx-auto" alt="" />
+
+        {/* ----------Gallery ---------- */}
+        <PhotoGallery> </PhotoGallery>
       </div>
      
     </div>
--- src/pages/Home/PhotoGallery/PhotoGallery.jsx
+++ src/pages/Home/PhotoGallery/PhotoGallery.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
-
+  
 const PhotoGallery = () => {
+    const image1 = 'https://jokomisiada.pl/eng_pl_Hasbro-Disney-Doll-Rapunzel-Tangled-ZA3642-15904_4.jpg';
+    const image2 = 'https://live.staticflickr.com/4184/34672508212_c570079b45_b.jpg';
+    const image3 = 'https://m.media-amazon.com/images/I/61sTdh33YZL._SX425_.jpg';
+    const image4 = 'https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/460022220242?fmt=jpeg&qlt=90&wid=652&hei=652&defaultImage=no-image-image_uk';
+    const image5 = 'https://www.toysrus.ca/dw/image/v2/BDFX_PRD/on/demandware.static/-/Sites-toys-master-catalog/default/dwd4e08d65/images/24D1A8FB_3.jpg?sw=767&sh=767&sm=fit';
+    const image6 = 'https://m.media-amazon.com/images/I/71e5Yd+YzML._AC_UF894,1000_QL80_.jpg';
+    const image7 = 'https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/460011777498?fmt=jpeg&qlt=90&wid=652&hei=652&defaultImage=no-image-image_uk';
+    const image8 = 'https://www.youloveit.com/uploads/posts/2022-02/1645172227_youloveit_com_disney_style_series_princess_jasmine.jpg';
+    const image9 = 'https://m.media-amazon.com/images/I/71fgaDOy8kS._AC_UF894,1000_QL80_.jpg';
+    const image10 = 'https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/460033511124-2?fmt=jpeg&qlt=90&wid=652&hei=652&defaultImage=no-image-image_uk';
+    const image11 = 'https://5.imimg.com/data5/EK/LW/NY/SELLER-6479327/jasmine-disney-princess-figurine-500x500.jpg';
+    const image12 = 'https://m.media-amazon.com/images/I/81t2O0EbsiL._AC_UF894,1000_QL80_.jpg';
+    const image13 = 'https://cf.shopee.ph/file/a4800c031ddda2dc6039d3dcab42f21e';
+    const image14 = 'https://m.media-amazon.com/images/I/61d1Wz1z4XS._AC_SY355_.jpg';
+    const image15 = 'https://eoms.cutpricebd.com/oms_products/big/164182003631438_33519.png';
+    const image16 = 'https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/460011777498?fmt=jpeg&qlt=90&wid=652&hei=652&defaultImage=no-image-image_uk';
+
     return (
-        <div>
-            
+        <div className='overflow-hidden'>
+           <div className='grid grid-cols-3 lg:grid-cols-4 gap-8 px-5 gallery mt-10'>
+            <div >
+                <img src={image1}  alt="" />
+            </div>
+            <div className=''>
+                <img src={image2} alt="" />
+            </div>
+            <div>
+                <img src={image3} alt="" />
+            </div>
+            <div>
+                <img src={image4} alt="" />
+            </div>
+            <div>
+                <img src={image5} alt="" />
+            </div>
+            <div>
+                <img src={image6} alt="" />
+            </div>
+            <div>
+                <img src={image7} alt="" />
+            </div>
+            <div>
+                <img src={image8} alt="" />
+            </div>
+            <div>
+                <img src={image9} alt="" />
+            </div>
+            <div>
+                <img src={image10} alt="" />
+            </div>
+            <div>
+                <img src={image11} alt="" />
+            </div>
+            <div>
+                <img src={image12} alt="" />
+            </div>
+            <div>
+                <img src={image13} alt="" />
+            </div>
+            <div>
+                <img src={image14} alt="" />
+            </div>
+            <div>
+                <img src={image15} alt="" />
+            </div>
+            <div>
+                <img src={image16} alt="" />
+            </div>
+           </div>
         </div>
     );
 };
